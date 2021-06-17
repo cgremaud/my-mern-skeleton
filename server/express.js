6107bc25364b8efd2ba5db58ee11e3d24f5413fb
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-17 11:11:50 -0500

    created user.model.js todo: configure mongoose schema

--- server/express.js
+++ server/express.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
+import Template from './../template'
 
 const app = express()
 
@@ -14,4 +15,8 @@ app.use(compress())
 app.use(helmet())
 app.use(cors())
  
+app.get('/', (req, res) => {
+    res.status(200).send(Template())
+})
+
 export default app
