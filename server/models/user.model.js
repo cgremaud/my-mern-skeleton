14ef6782adf49c495df8fd56bc641ff0abff0b77
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-17 11:26:49 -0500

    added basic fields to user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,23 @@
 import mongoose from 'mongoose'
 
-const UserSchema = new mongoose.Schema({})
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        requred: 'Name is required'
+    },
+    Email: {
+        type: String,
+        trim: true,
+        unique: 'Email alread exists',
+        match:  [/.+\@.+\..+/, 'Please fill a valid email address'],
+        required: 'Email is required'
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: Date //I guess you don't need the {} if it's only one k/v pair? 
+}) //creates a new schema to map js user objects onto mongoDB documents. takes an object as a schema defition
 
 export default mongoose.model('User', UserSchema)
