14fab900cf48c94f07384165eb5eb3432afed760
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-17 18:49:59 -0500

    added update method to user controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -49,4 +49,19 @@ const read = (req, res) => {
     req.profile.salt = undefined
     return res.json(req.profile)
 }
-   
+
+const update = async (req, res) => {
+    try {
+        let user = req.profile
+        user = extend(user, req.body)
+        user.updated = Date.now()
+        await user.save()
+        user.hashed_password = undefined
+        user.salt = undefined
+        res.json(user)
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}   
