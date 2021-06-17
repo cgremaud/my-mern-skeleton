2cd51684478321533417b1d379da22afcc0c5ff5
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-17 18:24:34 -0500

    added create method to user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,18 @@
+import User from '../models/user.model'
+//lodash helps update existing entries in db
+import extend from 'lodash/extend'
+import errorHandler from './error.controller'
+
+const create = async (req, res, next) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        return res.status(200).json({
+            message: "Successfully signed up!"
+        })
+    } catch (err) {
+        return res.status(400).json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
+}
